66906d35433e32ccd224e98487e73d95abe12161
Shaliya1908 <158029226+Shaliya1908@users.noreply.github.com>
2025-07-27 15:43:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Score Calculator App</h1>
+      <CalculateScore name="shals" school="Greenwood High" total={450} goal={5} />
+    </div>
+  );
+}
+
+export default App;
